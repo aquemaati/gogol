18c566963f731a311f95a5597f8ad9afe4c28f0d
aquemaati <aquemaati@gmail.com>
2024-03-07 13:39:58 +0100

    refactor 2

--- cmd/new/basic/basic.go
+++ cmd/new/basic/basic.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 MATHIAS MARCHETTI aquemaati@gmail.com
+Package cmd handles the basic command for creating simple projects to learn or test ideas.
 */
 package cmd
 
@@ -9,10 +9,7 @@ import (
 	"gogol/internal/tools"
 	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
 	"runtime"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -21,141 +18,81 @@ import (
 var BasicCmd = &cobra.Command{
 	Use:   "basic",
 	Short: "Create a simple project to learn or test ideas",
-	Long:  `Language avaiable : go, python, julia, ...`,
+	Long:  `Languages available: Go, Python, Julia, ...`,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		// Sarter message
+		// Starting message
 		fmt.Println(messages.StartingProject(cmd.Use, name, lang))
 
-		// STEP 1
-		// get general datas for basics projects with the
-		// programming language selected
+		// STEP 1: Get general data for basic projects with the selected programming language
 		data, err := tools.GetDatas("basic", lang)
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		// STEP 2
-		// get command to check if the programming language
-		// is properly installed
-		//TODO include in LangIsInstalled
+		// STEP 2: Get command to check if the programming language is properly installed
 		setUp, _ := tools.GetCmdCheckInstall(data.LinkSetup)
 
-		// STEP 3
-		// check if the language is wel installed, if not
-		// Show what the user have to do
+		// STEP 3: Check if the language is installed properly; show instructions if not
 		arch := runtime.GOARCH
 		ops := runtime.GOOS
 		outs, f, _ := tools.LangIsInstalled(setUp.CheckCommand[ops], lang)
 		fmt.Println(outs)
 		if !f {
 			fmt.Println(messages.ErrLangInstall(lang, ops, arch))
-			// allow the user to download what he need
-			// TODO check if brew or choco is installed
 			err := tools.HandleSetUp(ops, arch, setUp)
 			if err != nil {
-				fmt.Println(err, "could not install properly the programming language")
+				fmt.Println(err, "could not install the programming language properly")
 				return
 			}
 		}
 
-		// STEP 4
-		// create de root directory for the project and then go inside
+		// STEP 4: Create the root directory for the project and navigate inside
 		fmt.Println(messages.DirBuilding(name))
 		if err := os.Mkdir(name, 0777); err != nil {
 			log.Fatalln(err)
 		}
-		// Going inside this directory to execute new cmd
 		if err := os.Chdir(name); err != nil {
 			log.Fatalln(err)
 		}
 
-		// STEP 5
-		// get json instructions
-
+		// STEP 5: Get JSON instructions
 		basic, err := GetBasicJson(data.Link)
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		//STEP 6
-		// Execute precommand
-
-		for key, cm := range basic.PreCmd {
-			if key == "init" && len(cm) != 0 {
-				for i, v := range cm {
-					if v == "%s" {
-						basic.PreCmd[key][i] = name
-					}
-				}
-				fmt.Println("")
-				executeCommand(cm)
-			} else if key == "dep" && len(cm) != 0 {
-				for _, d := range dep {
-					cm = append(cm, d)
-					fmt.Println(messages.Fetching(d))
-					executeCommand(cm)
-				}
-			} else {
-				continue
-			}
+		// STEP 6: Execute pre-commands
+		if err := ExecutePreCommands(basic.PreCmd, name); err != nil {
+			log.Fatalln(err)
 		}
 
-		// STEP 7
-		// Create files and write inside
-		for _, f := range basic.Files {
-			file, err := os.Create(f.Name)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			defer file.Close()
-			// fmt.Printf("+++> Creating file : %s\n", f.Name)
-			fmt.Println(messages.FileBuilding(f.Name))
-			// Writing inside
-			text := strings.ReplaceAll(f.Content, "%s", name)
-			if _, err := file.WriteString(text); err != nil {
-				log.Fatalln(err)
-			}
+		// STEP 7: Create files and write inside
+		if err := CreateAndWriteFiles(basic.Files, name); err != nil {
+			log.Fatalln(err)
 		}
-		// STEP 8
-		// Create all subdirectories
+
+		// STEP 8: Create all subdirectories
 		if err := CreateFilesAndPackages(basic.Subdirectories, name); err != nil {
 			log.Fatalln(err)
 		}
 		fmt.Println("")
 
-		// STEP 9
-		// Add more files as asked by the user with parent directories
-		// Back to main repertory
+		// STEP 9: Add more files as requested by the user with parent directories
 		if err := ChangeToParentDirectoryWithSpecificName(name); err != nil {
 			log.Fatalln(err)
 		}
-		fmt.Println("files to add", addf)
-		for _, filePath := range addf {
-
-			err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
-			if err != nil {
-				fmt.Println("Erreur lors de la création des répertoires parents:", err)
-				return
-			}
-			// Crée le fichier
-			file, err := os.Create(filePath)
-			if err != nil {
-				fmt.Println("Erreur lors de la création du fichier:", err)
-				return
-			}
-			fmt.Println(messages.FileBuilding(filePath))
-			defer file.Close()
+		fmt.Println("Files to add:", addf)
+		if err := CreateAdditionalFiles(addf); err != nil {
+			log.Fatalln(err)
 		}
 
-		// SAY everything went ok
-		// fmt.Println("\nCONGRATULATION", cmd.Use, name, "Has been created successfully!!!")
-		// // Print last instructions
+		// Print final instructions
 		fmt.Println(messages.Congrat(cmd.Use, name, lang))
-		fmt.Print("\t\t Access your new directory: cd ", name, "\n")
+		fmt.Print("\t\t - Access your new directory: cd ", name, "\n")
 		for _, v := range basic.EndInstruction {
-			fmt.Println("\t\t", v)
+			fmt.Println("\t\t", "-", v)
 		}
+		fmt.Println("")
 	},
 }
 
@@ -163,60 +100,9 @@ var lang, name string
 var dep, addf []string
 
 func init() {
-	// Here you will define your flags and configuration settings.
+	// Define flags
 	BasicCmd.Flags().StringVarP(&lang, "lang", "l", "none", "Specify the programming language")
 	BasicCmd.Flags().StringVarP(&name, "name", "n", "project", "Specify the name")
-	BasicCmd.Flags().StringArrayVarP(&dep, "dep", "d", []string{}, "specify dependacies")
-	BasicCmd.Flags().StringArrayVarP(&addf, "addf", "", []string{}, "specify file to add")
-}
-
-func executeCommand(cm []string) {
-	fmt.Println("")
-	// fmt.Print("***> Executing command : ")
-
-	// Exécute la commande et récupère la sortie combinée
-	out, err := exec.Command(cm[0], cm[1:]...).CombinedOutput()
-	fmt.Println(messages.ExecCmd(strings.Join(cm, " "), string(out)))
-	// Vérifie s'il y a une erreur lors de l'exécution de la commande
-	if err != nil {
-		fmt.Println("\n", string(out))
-		fmt.Println("ERROR : while executing commands")
-		log.Fatalln(err)
-	}
-}
-
-// allow to come back to the main directory
-func ChangeToParentDirectoryWithSpecificName(targetDir string) error {
-	// Get the absolute path of the current directory
-	currentDir, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
-	// Iterate through parent directories until the target directory is found
-	for {
-		// Check if the current directory is the target directory
-		if filepath.Base(currentDir) == targetDir {
-			// If so, you are in the correct directory, no need to go further up
-			break
-		}
-
-		// Move up one directory level
-		parentDir := filepath.Dir(currentDir)
-		if parentDir == currentDir {
-			// If the current directory is already the root, stop the search
-			return fmt.Errorf("target directory not found: %s", targetDir)
-		}
-
-		// Update the current directory to the parent directory
-		currentDir = parentDir
-	}
-
-	// Change the working directory to the target directory
-	if err := os.Chdir(currentDir); err != nil {
-		return err
-	}
-
-	fmt.Println("Working directory updated:", currentDir)
-	return nil
+	BasicCmd.Flags().StringArrayVarP(&dep, "dep", "d", []string{}, "Specify dependencies")
+	BasicCmd.Flags().StringArrayVarP(&addf, "addf", "", []string{}, "Specify files to add")
 }
--- cmd/new/basic/constructor.go
+++ cmd/new/basic/constructor.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"gogol/internal/messages"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -39,5 +41,100 @@ func CreateFilesAndPackages(subdirs []Subdirectory, name string) error {
 		}
 		CreateFilesAndPackages(subdir.Subdirectories, name)
 	}
+	return nil
+}
+
+func ExecutePreCommands(preCmd map[string][]string, name string) error {
+	// Execute pre-commands
+	for key, cm := range preCmd {
+		if key == "init" && len(cm) != 0 {
+			for i, v := range cm {
+				if v == "%s" {
+					cm[i] = name
+				}
+			}
+			if err := ExecuteCommand(cm); err != nil {
+				return err
+			}
+		} else if key == "dep" && len(cm) != 0 {
+			for _, d := range dep {
+				cm = append(cm, d)
+				fmt.Println(messages.Fetching(d))
+				if err := ExecuteCommand(cm); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func CreateAndWriteFiles(files []File, name string) error {
+	// Create files and write inside
+	for _, f := range files {
+		file, err := os.Create(f.Name)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		fmt.Println(messages.FileBuilding(f.Name))
+		text := strings.ReplaceAll(f.Content, "%s", name)
+		if _, err := file.WriteString(text); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func ExecuteCommand(cm []string) error {
+	// Execute the command
+	cmd := exec.Command(cm[0], cm[1:]...)
+	out, err := cmd.CombinedOutput()
+	fmt.Println(messages.ExecCmd(strings.Join(cm, " "), string(out)))
+	if err != nil {
+		fmt.Println("\n", string(out))
+		fmt.Println("ERROR: while executing commands")
+		return err
+	}
+	return nil
+}
+
+func CreateAdditionalFiles(addf []string) error {
+	// Create additional files
+	for _, filePath := range addf {
+		err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm)
+		if err != nil {
+			return err
+		}
+		file, err := os.Create(filePath)
+		if err != nil {
+			return err
+		}
+		fmt.Println(messages.FileBuilding(filePath))
+		defer file.Close()
+	}
+	return nil
+}
+
+// ChangeToParentDirectoryWithSpecificName moves to the parent directory with the specified name
+func ChangeToParentDirectoryWithSpecificName(targetDir string) error {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	for {
+		if filepath.Base(currentDir) == targetDir {
+			break
+		}
+		parentDir := filepath.Dir(currentDir)
+		if parentDir == currentDir {
+			return fmt.Errorf("target directory not found: %s", targetDir)
+		}
+		currentDir = parentDir
+	}
+	if err := os.Chdir(currentDir); err != nil {
+		return err
+	}
+	fmt.Println("Working directory updated:", currentDir)
 	return nil
 }
